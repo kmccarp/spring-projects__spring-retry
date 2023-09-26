a90c910a39ef072bf12e67e3b907fbc680a82be2
Kevin Carpenter™️ <kevin@moderne.io>
2023-09-26 13:15:12 +0000

    refactor: Common static analysis issues
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.staticanalysis.CommonStaticAnalysis?organizationId=RGVmYXVsdA%3D%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/springframework/retry/policy/SimpleRetryPolicy.java
+++ src/main/java/org/springframework/retry/policy/SimpleRetryPolicy.java
@@ -65,7 +65,7 @@ public class SimpleRetryPolicy implements RetryPolicy {
 	/**
 	 * The default limit to the number of attempts for a new policy.
 	 */
-	public final static int DEFAULT_MAX_ATTEMPTS = 3;
+	public static final int DEFAULT_MAX_ATTEMPTS = 3;
 
 	private int maxAttempts;
 
@@ -235,7 +235,7 @@ public class SimpleRetryPolicy implements RetryPolicy {
 	 */
 	@Override
 	public void registerThrowable(RetryContext context, Throwable throwable) {
-		SimpleRetryContext simpleContext = ((SimpleRetryContext) context);
+		SimpleRetryContext simpleContext = (SimpleRetryContext) context;
 		simpleContext.registerThrowable(throwable);
 	}
 
